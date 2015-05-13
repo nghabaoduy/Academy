804bc9fbb566e8d9c18cac842d38842bf98111a9
brianlin1992 <brianlin1992@yahoo.com>
2015-05-13 21:45:55 +0700

    init project

---
+++ academy/academy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  academy
+//
+//  Created by Brian on 5/13/15.
+//  Copyright (c) 2015 Openlabproduction. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ academy/academy/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  academy
+//
+//  Created by Brian on 5/13/15.
+//  Copyright (c) 2015 Openlabproduction. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
